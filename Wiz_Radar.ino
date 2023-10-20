4aa766c36e80b9555c6767c53a7322e17e92d204
Ignacio <ignacio.degregori@gmail.com>
2023-10-20 19:37:26 -0300

    feat: light control
    add initial light control POC

--- Wiz_Radar.ino
+++ Wiz_Radar.ino
@@ -1,11 +1,17 @@
 #include <WiFi.h>
 #include <WiFiUdp.h>
 
+void sendBroadcast();
+
+const char* onString = "{\"id\":1,\"method\":\"setState\",\"params\":{\"state\": true }}";
+const char* offString = "{\"id\":1,\"method\":\"setState\",\"params\":{\"state\": false }}";
 const char* ssid = "";
 const char* password = "";
 
 WiFiUDP udp;
 byte ipArr[10];
+
+bool state = 0;
 
 unsigned int localPort = 38899;
 
@@ -20,21 +26,50 @@ void setup() {
   Serial.println("Connected to WiFi");
   
   udp.begin(localPort);
+
+  sendBroadcast();
 }
 
 void loop() {
-  sendBroadcast();
-    for (byte i = 0; i <= 10; i++) {
-    Serial.println(ipArr[i]);
-    delay(10);
+    // udp.beginPacket(IPAddress(255,255,255,255), localPort);
+    // if(state){
+    //   udp.print(onString);
+    //   state = 0;
+    // }else {
+    //   udp.print(offString);
+    //   state = 1;
+    // }
+    // udp.endPacket();
+    int il = 0;
+    while (il <= 9) {
+      Serial.println(il, ipArr[il]);
+      if(ipArr[il] != 0 ) {
+        udp.beginPacket(IPAddress(192,168,1,ipArr[il]), localPort);
+        if(state){
+          udp.print(onString);
+        }else {
+          udp.print(offString);
+        }
+        Serial.println(IPAddress(192,168,1,ipArr[il]).toString().c_str());
+        udp.endPacket();
+        delay(2000);
+      }
+      il = il + 1;
+    }
+  Serial.println("end of send");
+  if(state){
+    state = 0;
+  } else {
+    state = 1;
   }
-  delay(10000);
+  delay(3000);
 }
 
 void sendBroadcast() {
   udp.beginPacket(IPAddress(255,255,255,255), localPort); // broadcast address
   udp.print("{\"method\":\"getPilot\",\"params\":{}}");
   udp.endPacket();
+  Serial.printf("sent discovery\n");
 
   delay(100); 
   
