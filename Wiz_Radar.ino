b2b7b42cc1d32e8dd6e35d56a3ac811f5444c06d
Ignacio <ignacio.degregori@gmail.com>
2023-10-21 12:04:10 -0300

    feat: radar implementation
    -add radar support
    -connect radar and interrupts

--- Wiz_Radar.ino
+++ Wiz_Radar.ino
@@ -1,22 +1,42 @@
 #include <WiFi.h>
 #include <WiFiUdp.h>
+#include <ld2410.h>
+#include "secrets.h"
+
+ld2410 radar;
+
+uint32_t lastReading = 0;
+bool radarConnected = false;
+
+#define RADAR_RX_PIN 12
+#define RADAR_TX_PIN 13
+#define RADAR_OUT_PIN 2
 
 void sendBroadcast();
+void setupRadar();
+void readRadar();
+void IRAM_ATTR ISR();
+void peopleInInterrupt();
+void peopleOutInterrupt();
 
 const char* onString = "{\"id\":1,\"method\":\"setState\",\"params\":{\"state\": true }}";
 const char* offString = "{\"id\":1,\"method\":\"setState\",\"params\":{\"state\": false }}";
-const char* ssid = "";
-const char* password = "";
+const char* ssid = WIFI_SSID;
+const char* password = WIFIPASS;
 
 WiFiUDP udp;
 byte ipArr[10];
 
 bool state = 0;
+bool lightState = 0;
+bool changeToHigh = 0;
+bool changeToLow = 0;
 
 unsigned int localPort = 38899;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
+  pinMode(RADAR_OUT_PIN, INPUT);
   WiFi.begin(ssid, password);
   
   while (WiFi.status() != WL_CONNECTED) {
@@ -28,41 +48,49 @@ void setup() {
   udp.begin(localPort);
 
   sendBroadcast();
+  delay(150);
+  setupRadar();
+  attachInterrupt(RADAR_OUT_PIN, ISR, CHANGE);
+}
+
+void IRAM_ATTR ISR(){
+  if (digitalRead(RADAR_OUT_PIN) == HIGH) {
+    changeToHigh = 1;
+  } else {
+    changeToLow = 1;
+  }
+}
+
+void peopleInInterrupt() {
+  if(lightState == 0) {
+    udp.beginPacket(IPAddress(255,255,255,255), localPort); // broadcast address
+    udp.print(onString);
+    udp.endPacket();
+    lightState = 1;
+  }
+  readRadar();
+}
+
+void peopleOutInterrupt() {
+  if(lightState == 1) {
+    udp.beginPacket(IPAddress(255,255,255,255), localPort);
+    udp.print(offString);
+    udp.endPacket();
+    lightState = 0;
+  }
 }
 
 void loop() {
-    // udp.beginPacket(IPAddress(255,255,255,255), localPort);
-    // if(state){
-    //   udp.print(onString);
-    //   state = 0;
-    // }else {
-    //   udp.print(offString);
-    //   state = 1;
-    // }
-    // udp.endPacket();
-    int il = 0;
-    while (il <= 9) {
-      Serial.println(il, ipArr[il]);
-      if(ipArr[il] != 0 ) {
-        udp.beginPacket(IPAddress(192,168,1,ipArr[il]), localPort);
-        if(state){
-          udp.print(onString);
-        }else {
-          udp.print(offString);
-        }
-        Serial.println(IPAddress(192,168,1,ipArr[il]).toString().c_str());
-        udp.endPacket();
-        delay(2000);
-      }
-      il = il + 1;
-    }
-  Serial.println("end of send");
-  if(state){
-    state = 0;
-  } else {
-    state = 1;
+  if(changeToHigh) {
+    Serial.println("presenceDetected");
+    peopleInInterrupt();
+    changeToHigh = 0;
   }
-  delay(3000);
+  if(changeToLow) {
+    Serial.println("presenceUndetected");
+    peopleOutInterrupt();
+    changeToLow = 0;
+  }
 }
 
 void sendBroadcast() {
@@ -71,7 +99,7 @@ void sendBroadcast() {
   udp.endPacket();
   Serial.printf("sent discovery\n");
 
-  delay(100); 
+  delay(150); 
   
   int packetSize = udp.parsePacket();
   byte p = 0;
@@ -83,6 +111,58 @@ void sendBroadcast() {
     ipArr[p] = udp.remoteIP()[3];
     packetSize = udp.parsePacket();
     p++;
+  }
+}
+
+void readRadar() {
+  radar.read();
+  if(radar.isConnected() && millis() - lastReading > 1000)  //Report every 1000ms
+  {
+    lastReading = millis();
+    if(radar.presenceDetected()) {
+      if(radar.stationaryTargetDetected())
+      {
+        Serial.print(F("Stationary target: "));
+        Serial.print(radar.stationaryTargetDistance());
+        Serial.print(F("cm energy:"));
+        Serial.print(radar.stationaryTargetEnergy());
+        Serial.print(' ');
+      }
+      if(radar.movingTargetDetected())
+      {
+        Serial.print(F("Moving target: "));
+        Serial.print(radar.movingTargetDistance());
+        Serial.print(F("cm energy:"));
+        Serial.print(radar.movingTargetEnergy());
+      }
+      Serial.println();
+    } else {
+              udp.beginPacket(IPAddress(255,255,255,255), localPort); // broadcast address
+        udp.print(offString);
+        udp.endPacket();
+      Serial.println(F("No target"));
+    }
+  }
+}
+
+void setupRadar() {
+  // radar.debug(Serial);
+  Serial2.begin(256000, SERIAL_8N1, RADAR_RX_PIN, RADAR_TX_PIN); //UART for monitoring the radar
+  delay(500);
+  Serial.print(F("LD2410 radar sensor initialising: "));
+  if(radar.begin(Serial2))
+  {
+    Serial.println(F("OK"));
+    Serial.print(F("LD2410 firmware version: "));
+    Serial.print(radar.firmware_major_version);
+    Serial.print('.');
+    Serial.print(radar.firmware_minor_version);
+    Serial.print('.');
+    Serial.println(radar.firmware_bugfix_version, HEX);
+  }
+  else
+  {
+    Serial.println(F("not connected"));
   }
 }
 
